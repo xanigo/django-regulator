cf6fa6e2b20c9bd4ddaa126a1ee8d0bd54fe7c3e
Xurvan <xurvan@tutanota.com>
2021-08-12 19:29:30 +0430

    Add type hints to setting variables

--- regulator/settings.py
+++ regulator/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
-REDIS_DB = settings.REGULATOR['REDIS_DB']
-DEFAULT_CALLS = settings.REGULATOR.get('DEFAULT_CALLS', 30)
-DEFAULT_PERIOD = settings.REGULATOR.get('DEFAULT_PERIOD', 60)
+REDIS_DB: int = settings.REGULATOR['REDIS_DB']
+DEFAULT_CALLS: int = settings.REGULATOR.get('DEFAULT_CALLS', 30)
+DEFAULT_PERIOD: int = settings.REGULATOR.get('DEFAULT_PERIOD', 60)
