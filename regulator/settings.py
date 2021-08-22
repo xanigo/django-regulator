668888f28282ca1c77c9ee05da8521f9424e1ef0
Xurvan <xurvan@tutanota.com>
2021-08-23 01:38:45 +0430

    Change default calls to 10

--- regulator/settings.py
+++ regulator/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
 REDIS_DB: int = settings.REGULATOR['REDIS_DB']
-DEFAULT_CALLS: int = settings.REGULATOR.get('DEFAULT_CALLS', 30)
+DEFAULT_CALLS: int = settings.REGULATOR.get('DEFAULT_CALLS', 10)
 DEFAULT_PERIOD: int = settings.REGULATOR.get('DEFAULT_PERIOD', 60)
